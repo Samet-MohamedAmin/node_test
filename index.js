f9c69e42b34d514ea9d7a20dfa81569a130314fb
mohamedamin <mohamedamin.samet@gmail.com>
2019-04-05 01:17:53 +0100

    add route welcome home

--- index.js
+++ index.js
@@ -17,11 +17,15 @@ app.get('/api/test', async (req, res) => {
     res.send(greeting);
 });
 
+app.get('/api/home', async (req, res) => {
+    res.send({message: 'Welcome home!'});
+});
+
 
 mongoose.connect(`mongodb+srv://samet:${process.env.MONGO_PASSWORD}@cluster0-qjiwa.mongodb.net/test?retryWrites=true`)
     .then(() => {console.log('Connected to mongo...')})
     .catch(err => console.error('Connection failed...'));
 
-    
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
