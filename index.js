a1ce3e4969d6d0a6b35bd0b339d80ee0da6c0d4e
mohamedamin <mohamedamin.samet@gmail.com>
2019-04-05 01:12:23 +0100

    associate with mongo

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+
 
 app.use(express.json());
 app.use(cors());
 
 
+const greetingSchema = new mongoose.Schema({greeting: String});
+const Greeting = mongoose.model('Greeting', greetingSchema);
 
-app.get('/api/test', (req, res) => {
-    res.send({'greeting': 'Welcome from node !'});
+
+app.get('/api/test', async (req, res) => {
+    const greeting = await Greeting.findOne();
+    res.send(greeting);
 });
 
+
+mongoose.connect(`mongodb+srv://samet:${process.env.MONGO_PASSWORD}@cluster0-qjiwa.mongodb.net/test?retryWrites=true`)
+    .then(() => {console.log('Connected to mongo...')})
+    .catch(err => console.error('Connection failed...'));
+
+    
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
